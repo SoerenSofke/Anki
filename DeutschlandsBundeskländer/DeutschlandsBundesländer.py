0d1a1d3af53a52ca99a10b5e3f0951868a35479b
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2021-12-19 22:31:06 +0100

    changes

--- DeutschlandsBundeskländer/DeutschlandsBundesländer.py
+++ DeutschlandsBundeskländer/DeutschlandsBundesländer.py
@@ -42,19 +42,20 @@ def generateAnki():
         css='.card {font-family: arial; font-size: 20px; text-align: center; color: black; background-color: white; }'
     )
 
+    question = '<style>#State_Niedersachsen {fill: crimson;}</style>' + Path('map.svg').read_text()
+    answer = '<style>#State_Niedersachsen {fill: crimson;}</style>' + Path('map.svg').read_text()
+
     aDeck.add_note(
         genanki.Note(
             model=aModel,
             fields=[                
-                'F:<br><embed type="image/svg+xml" src="button.svg?fill=green" />',
-                'A:<br><embed type="image/svg+xml" src="button.svg?fill=green" />',
+                'Question <br>' + question,
+                'Answer <br>' + answer,
                 ]
         ))
 
-    filenames = ['button.svg']
-
     aPackage = genanki.Package(aDeck)
-    aPackage.media_files = filenames
+    aPackage.media_files = ['map.svg']
     aPackage.write_to_file(title + '.apkg')
 
 
